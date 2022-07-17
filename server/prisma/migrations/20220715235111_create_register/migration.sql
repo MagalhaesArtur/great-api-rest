8f23938ed3e30cc5f5b87ac79d08882657dc3893
João Artur Magalhães <joaoartur4590@gmail.com>
2022-07-17 08:22:24 -0300

    postgresql table adjust

--- server/prisma/migrations/20220715235111_create_register/migration.sql
+++ server/prisma/migrations/20220715235111_create_register/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "registros" (
     "nome" TEXT NOT NULL,
     "cpf" TEXT NOT NULL PRIMARY KEY ,
     "rg" TEXT NOT NULL,
-    "dataNasc" TEXT NOT NULL,
-    "nomeMae" TEXT NOT NULL,
-    "dataCadas" TEXT NOT NULL
+    "datanasc" TEXT NOT NULL,
+    "nomemae" TEXT NOT NULL,
+    "datacadas" TEXT NOT NULL
 );
