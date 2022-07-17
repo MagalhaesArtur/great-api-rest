67cb8e9b090187e2d235f09530f4ba55edb61c4d
Joao Artur <joaoartur4590@gmail.com>
2022-07-17 19:02:42 -0300

    apdate front end style

--- api-rest-great-front/src/App.tsx
+++ api-rest-great-front/src/App.tsx
@@ -7,7 +7,7 @@ import SearchUserByName from "./components/searchUserByName";
 
 function App() {
   return (
-    <div className="w-[100%] min-h-full  flex justify-center bg-roxin-500 items-center">
+    <div className="w-[100%] min-h-[100vh]  flex justify-center bg-roxin-500 items-center">
       <Router>
         <Routes>
           <Route path="/" element={<MainPageButtons />} />
