1e6ef3961aa9e42e7c16332612237cded34bf26d
Joao Artur <joaoartur4590@gmail.com>
2022-07-18 20:18:21 -0300

    formato de data att

--- api-rest-great-front/src/components/confirmDelete.tsx
+++ api-rest-great-front/src/components/confirmDelete.tsx
@@ -4,8 +4,8 @@ import { CircleNotch } from "phosphor-react";
 
 function ConfirmDelete(props: any) {
   return (
-    <div className="right-[50vw] top-[50vh] absolute  bg-green-500 opacity-40 flex justify-center items-center">
-      <div className="text-center bg-nsei-500 rounded-lg flex flex-col text-white font-bold">
+    <div className=" flex justify-center items-center">
+      <div className="text-center right-[50vw] top-[50vh] absolute w-[150px] bg-nsei-500 rounded-lg flex flex-col text-white font-bold">
         Aperte no botão confirmar e depois aperte em apagar usuário novamente
         para confirmar a deleção
         <button
