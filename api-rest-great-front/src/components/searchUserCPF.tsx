690408443a65810d5b173c1661c28fadf2e3b089
João Artur Magalhães <joaoartur4590@gmail.com>
2022-07-19 19:52:28 -0300

    rg must be unique

--- api-rest-great-front/src/components/searchUserCPF.tsx
+++ api-rest-great-front/src/components/searchUserCPF.tsx
@@ -75,6 +75,7 @@ function SearchUserCPF() {
             type="submit"
             className="p-2 w-[150px] rounded-xl font-bold bg-verdin-500 border-2 border-transparent text-roxin-500 hover:bg-nsei-500 hover:border-verdin-500 hover:text-verdin-500 transition-all"
             onClick={async () => {
+              validate();
               setLoading(true);
               try {
                 await axios
