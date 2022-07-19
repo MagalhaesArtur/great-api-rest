b88ae25b4ae03cc8e783bbe59956ae5797a0dd30
João Artur Magalhães <joaoartur4590@gmail.com>
2022-07-19 17:44:49 -0300

    rg must be unique

--- server/prisma/migrations/20220715235111_create_register/migration.sql
+++ server/prisma/migrations/20220715235111_create_register/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "registros" (
     "nome" TEXT NOT NULL,
     "cpf" TEXT NOT NULL PRIMARY KEY ,
-    "rg" TEXT NOT NULL,
+    "rg" TEXT NOT NULL UNIQUE,
     "datanasc" TEXT NOT NULL,
     "nomemae" TEXT NOT NULL,
     "datacadas" TEXT NOT NULL
