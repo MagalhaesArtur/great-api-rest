d935a12d80f518d25527007c28c4422761ff5cf9
João Artur Magalhães <joaoartur4590@gmail.com>
2022-09-07 12:20:32 -0300

    port number change

--- server/src/server.ts
+++ server/src/server.ts
@@ -25,38 +25,41 @@ app.post("/user", async (req, res) => {
 
   return res.status(201).json({ data: register });
 });
+try {
+  app.delete("/delete", async (req, res) => {
+    const { cpf } = req.body;
 
-app.delete("/delete", async (req, res) => {
-  const { cpf } = req.body;
+    const deletedUser = await prisma.registro.delete({ where: { cpf } });
 
-  const deletedUser = await prisma.registro.delete({ where: { cpf } });
-
-  return res.status(200).json({ data: deletedUser });
-});
-
-app.get("/delete", async (req, res) => {
-  const users = await prisma.registro.findMany();
-
-  return res.status(200).json({ users });
-});
-
-app.get("/user", async (req, res) => {
-  const cpf = req.params;
-
-  const register = await prisma.registro.findMany({
-    where: { cpf },
+    return res.status(200).json({ data: deletedUser });
   });
-  res.json(register);
-});
 
-app.get("/users", async (req, res) => {
-  const { nome } = req.body;
+  app.get("/delete", async (req, res) => {
+    const users = await prisma.registro.findMany();
 
-  const register = await prisma.registro.findMany({
-    where: { nome },
+    return res.status(200).json({ users });
   });
-  res.json(register);
-});
+
+  app.get("/user", async (req, res) => {
+    const cpf = req.params;
+
+    const register = await prisma.registro.findMany({
+      where: { cpf },
+    });
+    res.json(register);
+  });
+
+  app.get("/users", async (req, res) => {
+    const { nome } = req.body;
+
+    const register = await prisma.registro.findMany({
+      where: { nome },
+    });
+    res.json(register);
+  });
+} catch (err) {
+  console.log(err);
+}
 
 app.listen(porta, () => {
   console.log("servidor rodando na porta " + porta);
